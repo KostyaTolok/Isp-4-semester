6423e51fc023e397c0954e89ced26eed053ede33
Kostya <tolok.kostya@gmail.com>
2021-05-15 19:51:27 +0300

    product detail page added

--- Lab3/products/models.py
+++ Lab3/products/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
-
     name = models.CharField(verbose_name='Имя категории', max_length=255)
     slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-
     category = models.ForeignKey(Category, verbose_name="Категория товара", on_delete=models.CASCADE)
     name = models.CharField(verbose_name="Название товара", max_length=255)
     slug = models.SlugField(unique=True)
@@ -21,3 +23,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug, 'category': self.category.slug})
