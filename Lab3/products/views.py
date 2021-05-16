64f7157ae020b43c0e1c208a64fa04d01137dacc
Kostya <tolok.kostya@gmail.com>
2021-05-16 18:22:42 +0300

    added addition to cart and cart logic

--- Lab3/products/views.py
+++ Lab3/products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic.detail import View, DetailView
 from .models import Product, Category
+from cart.models import Cart, CartProduct
 
 
 class HomeView(View):
@@ -10,13 +12,27 @@ class HomeView(View):
         return render(request, 'shop/home.html', {'products': self.products})
 
 
-def index(request):
-    products = Product.objects.all()
-    return render(request, 'shop/home.html', {'products': products})
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        cart = Cart.objects.get(user=request.user)
+        product = Product.objects.get(slug=kwargs.get('slug'))
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.user, cart=cart, product=product,
+            overall_price=product.price
+        )
+        if created:
+            cart.products.add(cart_product)
+        else:
+            cart_product.quantity += 1
+            cart_product.save()
+        cart.products_amount += 1
+        cart.overall_price += cart_product.product.price
+        cart.save()
+        return HttpResponseRedirect('/cart/')
 
 
 class ProductDetailView(DetailView):
-
     model = Product
     queryset = Product.objects.all()
     template_name = 'shop/product_detail.html'
@@ -27,7 +43,6 @@ class ProductDetailView(DetailView):
 
 
 class CategoryDetailView(DetailView):
-
     model = Category
     queryset = Category.objects.all()
     template_name = "shop/category_detail.html"
