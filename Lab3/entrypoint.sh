f1cdfb93d47cf14b62cc119e698abd9b840c0c1c
Kostya <tolok.kostya@gmail.com>
2021-06-08 21:01:21 +0300

    added super user creating

--- Lab3/entrypoint.sh
+++ Lab3/entrypoint.sh
@@ -11,5 +11,9 @@ done
 echo "Applying database migrations"
 python manage.py migrate
 
+echo "Creating superuser"
+python manage.py createsuperuser --noinput
+
+
 echo "Starting server on 0.0.0.0:8000"
 python manage.py runserver 0.0.0.0:8000
